7bd40a343c02734a8b4fa53ea8c360663ddb3221
Dariia96 <darynastakhnyak@gmail.com>
2023-05-29 22:18:20 +0300

    new initial commit

--- src/service/fetchCountries.js
+++ src/service/fetchCountries.js
@@ -1,19 +1,2 @@
-import Notiflix from 'notiflix';
-function fetchCountries(country) {
 
-const BASE_URL = "https://restcountries.com/v3.1/name";
-
-        return fetch(`${BASE_URL}/${country}`)
-            .then(resp => {
-                console.log(resp);
-                if (!resp.ok) {
-                    throw new Error(resp.statusText);
-                } 
-                return resp.json() 
-            })
-            
-    
-}
-
-export { fetchCountries }
 
